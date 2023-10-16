1ca55f2d7fbd51d5097fc34697f8c65b6ef913a5
Warren Byron <20091892@mail.wit.ie>
2023-10-16 16:42:03 +0100

    Create 10-metrics-table.sql

---
+++ DDL/10-metrics-table.sql
@@ -0,0 +1,15 @@
+CREATE TABLE metrics (
+    metric_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    weight DECIMAL(5, 2),
+    height DECIMAL(5, 2),
+    bmi DECIMAL(5, 2),
+    systolic_blood_pressure INT,
+    diastolic_blood_pressure INT,
+    heart_rate INT,
+    blood_sugar DECIMAL(5, 2),
+    sleep_duration INT,
+    sleep_quality VARCHAR(20),
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
