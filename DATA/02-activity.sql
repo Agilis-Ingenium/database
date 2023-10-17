d812f262c2543a02f91c25aec0f8f5c619a8baba
Warren Byron <20091892@mail.wit.ie>
2023-10-17 18:36:32 +0100

    Update 02-activity.sql

--- DATA/02-activity.sql
+++ DATA/02-activity.sql
@@ -1,11 +1,11 @@
 INSERT INTO activity_data (user_id, activity_type, duration_minutes, distance_km, workout_intensity, activity_date) 
 VALUES 
-    (1, 'yoga', 30, NULL, 'low', '2023-09-15'),
-    (2, 'running', 45, 5.2, 'moderate', '2023-09-14'),
-    (3, 'swimming', 60, 1.5, 'high', '2023-09-13'),
-    (4, 'strength training', 60, NULL, 'high-intensity', '2023-09-12'),
-    (5, 'aerobic exercise', 30, 10, 'high', '2023-09-11'),
-    (6, 'walking', 45, 3.5, 'moderate', '2023-09-10'),
-    (7, 'bicycling', 60, 20, 'moderate', '2023-09-09'),
-    (8, 'weightlifting', 45, NULL, 'moderate-intensity', '2023-09-08'),
-    (9, 'yoga', 30, NULL, 'low', '2023-09-07');
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'yoga', 30, NULL, 'low', '2023-09-15'),
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'running', 45, 5.2, 'moderate', '2023-09-14'),
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'swimming', 60, 1.5, 'high', '2023-09-13'),
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'strength training', 60, NULL, 'high-intensity', '2023-09-12'),
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'aerobic exercise', 30, 10, 'high', '2023-09-11'),
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'walking', 45, 3.5, 'moderate', '2023-09-10'),
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'bicycling', 60, 20, 'moderate', '2023-09-09'),
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'weightlifting', 45, NULL, 'moderate-intensity', '2023-09-08'),
+    ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'yoga', 30, NULL, 'low', '2023-09-07');
