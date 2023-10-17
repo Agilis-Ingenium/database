9955c1e17a729ba5f71d4bec55ee01cc00c1cd1c
Warren Byron <20091892@mail.wit.ie>
2023-10-17 16:41:14 +0100

    Update 20-workout-plans-table.sql

--- DDL/20-workout-plans-table.sql
+++ DDL/20-workout-plans-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE workout_plans (
     plan_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id INT NOT NULL,
+    user_id uuid NOT NULL,
     plan_name VARCHAR(255) NOT NULL,
     schedule VARCHAR(255),
     plan_date DATE,
