b1c59d88a7c4bd12dd14dbcb6d47b4c1a0b0c103
Warren Byron <20091892@mail.wit.ie>
2023-10-17 16:09:30 +0100

    Update 45-activity-data-table.sql

--- DDL/45-activity-data-table.sql
+++ DDL/45-activity-data-table.sql
@@ -5,7 +5,7 @@ CREATE TABLE activity_data (
     user_id INT NOT NULL,
     activity_type activity_type NOT NULL,
     duration_minutes INT NOT NULL,
-    distance_km DECIMAL(8, 2),
+    distance_km NUMERIC(2),
     workout_intensity VARCHAR(50),
     activity_date DATE NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
