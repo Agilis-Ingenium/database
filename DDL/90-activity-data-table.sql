b02fecbcf44c87274544ee5664f46d1e0c9fd136
Warren Byron <20091892@mail.wit.ie>
2023-10-17 08:30:48 +0100

    Create 90-activity-data-table.sql

---
+++ DDL/90-activity-data-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE activity_data (
+    activity_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    activity_type VARCHAR(255) NOT NULL,
+    duration_minutes INT NOT NULL,
+    distance_miles DECIMAL(8, 2),
+    workout_intensity VARCHAR(50),
+    activity_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
