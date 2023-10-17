a8fb0adabecb1f474d2985fd9a3f0975ce838115
Warren Byron <20091892@mail.wit.ie>
2023-10-17 08:21:28 +0100

    Create 40-workout-plans-table.sql

---
+++ DDL/40-workout-plans-table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE workout_plans (
+    plan_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    plan_name VARCHAR(255) NOT NULL,
+    schedule VARCHAR(255),
+    plan_date DATE,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
