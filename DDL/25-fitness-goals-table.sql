1229ef48415b7c7bc185f12bdbfc46bbc72021cd
Warren Byron <20091892@mail.wit.ie>
2023-10-17 16:41:34 +0100

    Update 25-fitness-goals-table.sql

--- DDL/25-fitness-goals-table.sql
+++ DDL/25-fitness-goals-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE fitness_goals (
     goal_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id INT NOT NULL,
+    user_id uuid NOT NULL,
     goal_type VARCHAR(255) NOT NULL,
     target_value DECIMAL(10, 2) NOT NULL,
     target_date DATE,
