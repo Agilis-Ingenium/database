42587766f6be96bff0f9795cbeb879c708bb05f6
Warren Byron <20091892@mail.wit.ie>
2023-10-17 17:04:41 +0100

    Update 43-workout-intensity-type.sql

--- DDL/43-workout-intensity-type.sql
+++ DDL/43-workout-intensity-type.sql
@@ -2,4 +2,4 @@ CREATE TYPE workout_intensity AS ENUM (
   'low', 'light', 'moderate', 'low to moderate', 
   'moderate-intensity', 'high', 'high-intensity', 
   'very high', 'extreme', 'varied'
-)
+);
