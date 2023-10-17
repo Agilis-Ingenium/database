0845137b7e61990594551847f40dc7469001a218
Warren Byron <20091892@mail.wit.ie>
2023-10-17 18:35:42 +0100

    Update 01-user-data.sql

--- DATA/01-user-data.sql
+++ DATA/01-user-data.sql
@@ -1,2 +1,1 @@
-INSERT INTO users VALUES (1, 'Homer Simpson', 'homer@simpson.com');
-INSERT INTO users VALUES (2, 'Marge Simpson', 'marge@simpson.com');
+INSERT INTO users VALUES ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'Homer Simpson', 'homer@simpson.com');
