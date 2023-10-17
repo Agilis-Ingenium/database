846536ca27f943e915dc1d26ad0cf05db9d3dc66
Warren Byron <20091892@mail.wit.ie>
2023-10-17 08:27:43 +0100

    Create 60-food-items-table.sql

---
+++ DDL/60-food-items-table.sql
@@ -0,0 +1,10 @@
+CREATE TABLE food_items (
+    food_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    calories INT NOT NULL,
+    carbohydrates DECIMAL(8, 2) NOT NULL,
+    proteins DECIMAL(8, 2) NOT NULL,
+    fats DECIMAL(8, 2) NOT NULL,
+    vitamins VARCHAR(255),
+    minerals VARCHAR(255)
+);
