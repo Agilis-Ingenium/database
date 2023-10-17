09d937f78c1a6ea7cd79fa9b48aa35ddafe22ff8
Warren Byron <20091892@mail.wit.ie>
2023-10-17 16:40:51 +0100

    Update 15-calorie-intake-table.sql

--- DDL/15-calorie-intake-table.sql
+++ DDL/15-calorie-intake-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE calorie_intake (
     intake_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id INT NOT NULL,
+    user_id uuid NOT NULL,
     food_item VARCHAR(255) NOT NULL,
     calories INT NOT NULL,
     intake_date DATE NOT NULL,
