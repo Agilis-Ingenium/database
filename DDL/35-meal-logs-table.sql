ee462c5a651e39e264a8be165b3f777566cd3f07
Warren Byron <20091892@mail.wit.ie>
2023-10-17 18:54:28 +0100

    Update 35-meal-logs-table.sql

--- DDL/35-meal-logs-table.sql
+++ DDL/35-meal-logs-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE meal_logs (
-    log_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
+    log_id SERIAL PRIMARY KEY,
     user_id uuid NOT NULL,
     meal_time TIMESTAMP NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
