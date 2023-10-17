1903e4fbc8c437f1cb3c52799d273cb8c6aebc5c
Warren Byron <20091892@mail.wit.ie>
2023-10-17 18:58:56 +0100

    Update 50-health-trends.sql

--- DDL/50-health-trends.sql
+++ DDL/50-health-trends.sql
@@ -1,6 +1,6 @@
 CREATE TABLE health_trends (
     trend_id SERIAL PRIMARY KEY,
-    user_id uuid NOT NULL,
+    user_id INT NOT NULL,
     metric_type VARCHAR(255) NOT NULL,
     trend_data JSON NOT NULL,
     trend_date DATE NOT NULL,
