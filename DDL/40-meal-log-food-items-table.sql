bc6ae840aa9cfc7cf5a55c2c86f4b69e9e95f939
Warren Byron <20091892@mail.wit.ie>
2023-10-17 18:58:13 +0100

    Update 40-meal-log-food-items-table.sql

--- DDL/40-meal-log-food-items-table.sql
+++ DDL/40-meal-log-food-items-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE meal_log_food_items (
-    meal_log_id uuid NOT NULL,
-    food_id uuid NOT NULL,
+    meal_log_id INT NOT NULL,
+    food_id INT NOT NULL,
     quantity DECIMAL(8, 2) NOT NULL,
     FOREIGN KEY (meal_log_id) REFERENCES meal_logs(log_id),
     FOREIGN KEY (food_id) REFERENCES food_items(food_id)
