42cafff04f07b36d435d10f468d6c3a9acf797fd
Warren Byron <20091892@mail.wit.ie>
2023-10-17 08:31:17 +0100

    Create 100-health-trends.sql

---
+++ DDL/100-health-trends.sql
@@ -0,0 +1,8 @@
+CREATE TABLE health_trends (
+    trend_id INT AUTO_INCREMENT PRIMARY KEY,
+    user_id INT NOT NULL,
+    metric_type VARCHAR(255) NOT NULL,
+    trend_data JSON NOT NULL,
+    trend_date DATE NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
