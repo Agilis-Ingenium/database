ea40b3f076b644b336fdd38c8d0e3d67f06e63c0
Warren Byron <20091892@mail.wit.ie>
2023-10-17 16:35:48 +0100

    Update 01-user-table.sql

--- DDL/01-user-table.sql
+++ DDL/01-user-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-   user_id INT NOT NULL,
+   user_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
    name VARCHAR (100) NOT NULL,
    email VARCHAR (255) UNIQUE NOT NULL
   );
