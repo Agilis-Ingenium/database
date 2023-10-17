eaa474841b44e0dad5af90d1b2c96d3816fa6109
Warren Byron <20091892@mail.wit.ie>
2023-10-17 16:40:29 +0100

    Update 10-exercise-logs-table.sql

--- DDL/10-exercise-logs-table.sql
+++ DDL/10-exercise-logs-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE exercise_logs (
     log_id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-    user_id INT NOT NULL,
+    user_id uuid NOT NULL,
     exercise_type VARCHAR(255) NOT NULL,
     duration_minutes INT NOT NULL,
     intensity VARCHAR(50),
