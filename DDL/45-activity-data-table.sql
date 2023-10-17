18a27a5aae52cce068f8f2bc597e6853603cb5cd
Warren Byron <20091892@mail.wit.ie>
2023-10-17 18:58:42 +0100

    Update 45-activity-data-table.sql

--- DDL/45-activity-data-table.sql
+++ DDL/45-activity-data-table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE activity_data (
     activity_id SERIAL PRIMARY KEY,
-    user_id uuid NOT NULL,
+    user_id INT NOT NULL,
     activity_type activity_type NOT NULL,
     duration_minutes INT NOT NULL,
     distance_km NUMERIC(2),
