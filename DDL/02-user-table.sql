6703f43b1c21b1459b5176d3b5ad35e0808db742
Warren Byron <20091892@mail.wit.ie>
2023-10-18 06:09:32 +0100

    Cleanup code

--- DDL/02-user-table.sql
+++ DDL/02-user-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE users (
-    user_id serial PRIMARY KEY,
+    user_id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
     password VARCHAR(255) NOT NULL,
