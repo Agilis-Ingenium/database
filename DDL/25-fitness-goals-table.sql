35a564bbd963fc88bd274be8e9fe900a9a4460ca
Warren Byron <20091892@mail.wit.ie>
2023-10-18 08:08:29 +0100

    Add enum goal_type field

--- DDL/25-fitness-goals-table.sql
+++ DDL/25-fitness-goals-table.sql
@@ -1,7 +1,7 @@
 CREATE TABLE fitness_goals (
     goal_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    goal_type VARCHAR(255) NOT NULL,
+    goal_type goal_type NOT NULL,
     target_value DECIMAL(10, 2) NOT NULL,
     target_date DATE,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
