eca6cd8c1ea41efd42e427a824f2f9662484328c
Warren Byron <20091892@mail.wit.ie>
2023-10-18 08:02:32 +0100

    Add calories column

--- DDL/40-meal-log-food-items-table.sql
+++ DDL/40-meal-log-food-items-table.sql
@@ -2,6 +2,7 @@ CREATE TABLE meal_log_food_items (
     meal_log_id INT NOT NULL,
     food_id INT NOT NULL,
     quantity DECIMAL(8, 2) NOT NULL,
+    calories INT,
     FOREIGN KEY (meal_log_id) REFERENCES meal_logs(log_id),
     FOREIGN KEY (food_id) REFERENCES food_items(food_id)
 );
