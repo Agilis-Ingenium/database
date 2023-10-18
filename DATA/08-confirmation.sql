660a0ac8e0ac550ddd7035fe6c479db3a3d42cce
Warren Byron <20091892@mail.wit.ie>
2023-10-18 12:49:08 +0100

    Write script to output all data for confirmation

---
+++ DATA/08-confirmation.sql
@@ -0,0 +1,23 @@
+-- Confirm Users Data
+SELECT * FROM users;
+
+-- Confirm Metrics Data
+SELECT * FROM metrics;
+
+-- Confirm Workout Plans Data
+SELECT * FROM workout_plans;
+
+-- Confirm Fitness Goals Data
+SELECT * FROM fitness_goals;
+
+-- Confirm Food Items Data
+SELECT * FROM food_items;
+
+-- Confirm Meal Logs Data
+SELECT * FROM meal_logs;
+
+-- Confirm Meal Log Food Items Data
+SELECT * FROM meal_log_food_items;
+
+-- Confirm Activity Data
+SELECT * FROM activity_data;
