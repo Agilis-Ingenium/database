edc212ba713da51d3abf77add6190325ba9a3ee1
Warren Byron <20091892@mail.wit.ie>
2023-10-18 08:04:24 +0100

    Remove redundant calorie_intake_table, data also in meals
    Add total_calories and calories per food item to meal_logs tables

--- DDL/15-calorie-intake-table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE calorie_intake (
-    intake_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    food_item VARCHAR(255) NOT NULL,
-    calories INT NOT NULL,
-    intake_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
