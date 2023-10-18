919794aa3b01d6affb0e8ff1b87f1a6708ebeb77
Warren Byron <20091892@mail.wit.ie>
2023-10-18 05:13:27 +0100

    Improve User Table

--- DDL/01-user-table.sql
+++ DDL/01-user-table.sql
@@ -1,5 +1,11 @@
 CREATE TABLE users (
-   user_id SERIAL PRIMARY KEY,
-   name VARCHAR (100) NOT NULL,
-   email VARCHAR (255) UNIQUE NOT NULL
-  );
+    user_id serial PRIMARY KEY,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    date_of_birth DATE,
+    gender VARCHAR(10),
+    registration_date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
