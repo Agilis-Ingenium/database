03e278df1c01ae8d937d107fe38a4d15cc4481a2
Warren Byron <20091892@mail.wit.ie>
2023-10-18 18:28:46 +0100

    Include ENUM on workout-plan table schedule field

--- DDL/20-workout-plans-table.sql
+++ DDL/20-workout-plans-table.sql
@@ -2,7 +2,7 @@ CREATE TABLE workout_plans (
     plan_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     plan_name VARCHAR(255) NOT NULL,
-    schedule VARCHAR(255),
+    schedule schedule,
     plan_date DATE,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
