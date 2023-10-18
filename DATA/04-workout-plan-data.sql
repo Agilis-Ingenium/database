477bf58235d28b456fa5379cedd1224435aa4e56
Warren Byron <20091892@mail.wit.ie>
2023-10-18 18:30:13 +0100

    Modify import data to support schedule type options

--- DATA/04-workout-plan-data.sql
+++ DATA/04-workout-plan-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO workout_plans (user_id, plan_name, schedule, plan_date)
 VALUES
-    (1, 'Weight Training Plan', 'Mon-Wed-Fri', '2023-09-30'),
-    (1, 'Cardio Workout Plan', 'Tue-Thu-Sat', '2023-10-01'),
-    (1, 'Cardio Workout Plan', 'Mon-Wed-Fri', '2023-10-01'),
-    (1, 'Strength Training Plan', 'Tue-Thu-Sat', '2023-10-03');
+    (1, 'Weight Training Plan', 'weekly', '2023-09-30'),
+    (1, 'Cardio Workout Plan', 'daily', '2023-10-01'),
+    (1, 'Cardio Workout Plan', 'bi-weekly', '2023-10-01'),
+    (1, 'Strength Training Plan', 'monthly', '2023-10-03');
