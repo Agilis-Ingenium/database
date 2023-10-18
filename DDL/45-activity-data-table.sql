1a6b3d5450d7af12dcc0d00bb941fe50221ea276
Warren Byron <20091892@mail.wit.ie>
2023-10-18 17:22:33 +0100

    Fix workout_intensity field - missing ENUM

--- DDL/45-activity-data-table.sql
+++ DDL/45-activity-data-table.sql
@@ -4,7 +4,7 @@ CREATE TABLE activity_data (
     activity_type activity_type NOT NULL,
     duration_minutes INT NOT NULL,
     distance_km NUMERIC(2),
-    workout_intensity VARCHAR(50),
+    workout_intensity workout_intensity NOT NULL,
     activity_date DATE NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
