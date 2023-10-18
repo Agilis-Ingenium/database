6fe430ac037d0abe8f17d2d5e581c74266511eeb
Warren Byron <20091892@mail.wit.ie>
2023-10-18 09:31:53 +0100

    Update 01-user-data.sql

--- DATA/01-user-data.sql
+++ DATA/01-user-data.sql
@@ -1,1 +1,6 @@
-INSERT INTO users VALUES ('bd18cec1-c15c-48a3-af14-6d406b887fbf', 'Homer Simpson', 'homer@simpson.com');
+INSERT INTO users (username, email, password, first_name, last_name, date_of_birth, gender)
+VALUES
+    ('mary_jones', 'mary@example.com', 'secure123', 'Mary', 'Jones', '1988-09-02', 'female'),
+    ('peter_wilson', 'peter@example.com', 'pass1234', 'Peter', 'Wilson', '1993-07-18', 'male'),
+    ('john_doe', 'john@example.com', 'password123', 'John', 'Doe', '1990-05-15', 'male'),
+    ('jane_smith', 'jane@example.com', 'secret456', 'Jane', 'Smith', '1985-12-10', 'female');
