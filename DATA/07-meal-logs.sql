cd33c3af5e0539411dcbf0cca3df9586d5fd0856
Warren Byron <20091892@mail.wit.ie>
2023-10-18 12:46:53 +0100

    Change all meal logs to user 1

--- DATA/07-meal-logs.sql
+++ DATA/07-meal-logs.sql
@@ -2,7 +2,7 @@
 INSERT INTO meal_logs (user_id, meal_time, total_calories)
 VALUES
     (1, '2023-09-15 12:30:00', 450),
-    (2, '2023-09-15 18:45:00', 600);
+    (1, '2023-09-15 18:45:00', 600);
 
 -- Insert data into the "meal_log_food_items" table
 INSERT INTO meal_log_food_items (meal_log_id, food_id, quantity, calories)
@@ -13,8 +13,8 @@ VALUES
 -- Insert more data into the "meal_logs" table
 INSERT INTO meal_logs (user_id, meal_time, total_calories)
 VALUES
-    (3, '2023-09-15 13:45:00', 480),
-    (4, '2023-09-15 19:30:00', 620);
+    (1, '2023-09-16 13:45:00', 480),
+    (1, '2023-09-17 19:30:00', 620);
 
 -- Insert more data into the "meal_log_food_items" table
 INSERT INTO meal_log_food_items (meal_log_id, food_id, quantity, calories)
