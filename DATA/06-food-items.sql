a04106f2440eef337cacbac097a1c7c7de94f088
Warren Byron <20091892@mail.wit.ie>
2023-10-18 12:26:06 +0100

    Food items table

---
+++ DATA/06-food-items.sql
@@ -0,0 +1,26 @@
+INSERT INTO food_items (name, calories, carbohydrates, proteins, fats, vitamins, minerals)
+VALUES
+    ('Chicken Breast', 165, 0.0, 31.0, 3.6, 'Vitamin B6, Vitamin B12', 'Iron, Zinc'),
+    ('Salmon', 206, 0.0, 22.0, 13.0, 'Vitamin D, Vitamin B12', 'Selenium, Omega-3 Fatty Acids'),
+    ('Broccoli', 55, 11.2, 3.7, 0.6, 'Vitamin C, Vitamin K', 'Folate, Potassium'),
+    ('Spinach', 23, 3.6, 2.9, 0.4, 'Vitamin A, Vitamin C', 'Iron, Folate'),
+    ('Eggs', 68, 0.6, 5.5, 4.8, 'Vitamin D, Vitamin B12', 'Choline, Selenium'),
+    ('Brown Rice', 216, 44.8, 5.0, 1.6, 'Thiamin, Niacin', 'Magnesium, Phosphorus'),
+    ('Banana', 105, 27.0, 1.3, 0.4, 'Vitamin C, Vitamin B6', 'Potassium, Manganese'),
+    ('Greek Yogurt', 100, 6.0, 10.0, 3.5, 'Probiotics, Calcium', 'Vitamin B12, Phosphorus'),
+    ('Almonds', 7, 2.3, 2.4, 6.1, 'Vitamin E, Riboflavin', 'Magnesium, Copper'),
+    ('Tofu', 144, 1.5, 15.0, 8.0, 'Iron, Calcium', 'Manganese, Selenium'),
+    ('Carrots', 41, 10.0, 1.0, 0.2, 'Vitamin A, Vitamin K', 'Potassium, Fiber'),
+    ('Oats', 71, 12.0, 2.5, 1.4, 'Fiber, Manganese', 'Phosphorus, Magnesium'),
+    ('Oranges', 43, 8.2, 1.0, 0.2, 'Vitamin C, Folate', 'Potassium, Fiber'),
+    ('Lentils', 230, 40.0, 18.0, 1.0, 'Folate, Iron', 'Phosphorus, Magnesium'),
+    ('Milk', 83, 12.0, 8.0, 1.0, 'Calcium, Vitamin D', 'Protein, Potassium'),
+    ('Quinoa', 120, 21.0, 4.1, 1.9, 'Protein, Fiber', 'Manganese, Magnesium'),
+    ('Apples', 52, 14.0, 0.3, 0.2, 'Vitamin C, Fiber', 'Potassium, Vitamin A'),
+    ('Tomatoes', 18, 3.9, 0.9, 0.2, 'Vitamin C, Vitamin K', 'Potassium, Folate'),
+    ('Avocado', 160, 8.5, 2.0, 14.7, 'Vitamin K, Folate', 'Potassium, Monounsaturated Fats'),
+    ('Green Peas', 81, 14.5, 4.5, 0.5, 'Vitamin C, Vitamin K', 'Folate, Fiber');
+    ('Salmon', 206, 0.0, 22.0, 13.0, 'Vitamin D, Vitamin B12', 'Selenium, Omega-3 Fatty Acids'),
+    ('Kale', 33, 6.7, 2.9, 0.6, 'Vitamin K, Vitamin A', 'Calcium, Iron');
+    ('Chicken Breast', 165, 0.0, 31.0, 3.6, 'Vitamin B6, Vitamin B12', 'Iron, Zinc'),
+    ('Broccoli', 55, 11.2, 3.7, 0.6, 'Vitamin C, Vitamin K', 'Folate, Potassium');
