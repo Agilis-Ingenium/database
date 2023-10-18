a3470cf61ff6878a6c0d7a286d2daed676090f59
Warren Byron <20091892@mail.wit.ie>
2023-10-18 12:21:25 +0100

    Workout plan data

---
+++ DATA/04-workout-plan-data.sql
@@ -0,0 +1,6 @@
+INSERT INTO workout_plans (user_id, plan_name, schedule, plan_date)
+VALUES
+    (3, 'Weight Training Plan', 'Mon-Wed-Fri', '2023-09-30'),
+    (4, 'Cardio Workout Plan', 'Tue-Thu-Sat', '2023-10-01');
+    (1, 'Cardio Workout Plan', 'Mon-Wed-Fri', '2023-10-01'),
+    (2, 'Strength Training Plan', 'Tue-Thu-Sat', '2023-10-03');
