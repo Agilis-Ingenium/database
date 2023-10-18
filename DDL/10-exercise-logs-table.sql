aad7ad80a84947106ab439209d77aee53cdf70ed
Warren Byron <20091892@mail.wit.ie>
2023-10-18 06:41:01 +0100

    Delete exercise_logs table as same as activity-data

--- DDL/10-exercise-logs-table.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE exercise_logs (
-    log_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    exercise_type VARCHAR(255) NOT NULL,
-    duration_minutes INT NOT NULL,
-    intensity VARCHAR(50),
-    log_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
