0f24457b2812970bb8be0b20d13a65440285bc92
Warren Byron <20091892@mail.wit.ie>
2023-10-18 09:16:53 +0100

    Remove health_trend table
    Seems to be redundant and trends can be found in the other data tables.

--- DDL/50-health-trends_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE health_trends (
-    trend_id SERIAL PRIMARY KEY,
-    user_id INT NOT NULL,
-    metric_type VARCHAR(255) NOT NULL,
-    trend_data JSON NOT NULL,
-    trend_date DATE NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(user_id)
-);
