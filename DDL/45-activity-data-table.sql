3c21b13f8c1dcffaa53c0d6dfa900f9c8111ae3a
Warren Byron <20091892@mail.wit.ie>
2023-10-26 12:59:26 +0100

    Remove ENUM types

--- DDL/45-activity-data-table.sql
+++ DDL/45-activity-data-table.sql
@@ -1,10 +1,10 @@
 CREATE TABLE activity_data (
     activity_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
-    activity_type activity_type NOT NULL,
+    activity_type VARCHAR(20) NOT NULL,
     duration_minutes INT NOT NULL,
     distance_km NUMERIC(2),
-    workout_intensity workout_intensity NOT NULL,
+    workout_intensity VARCHAR(20) NOT NULL,
     activity_date DATE NOT NULL,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
