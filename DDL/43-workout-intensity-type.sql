7788d3caa74516bd1ddb05ec4c81d195e41bf417
Warren Byron <20091892@mail.wit.ie>
2023-10-26 12:58:06 +0100

    Remove ENUM types

--- DDL/43-workout-intensity-type.sql
+++
@@ -1,5 +0,0 @@
-CREATE TYPE workout_intensity AS ENUM (
-  'low', 'light', 'moderate', 'low to moderate', 
-  'moderate-intensity', 'high', 'high-intensity', 
-  'very high', 'extreme', 'varied'
-);
