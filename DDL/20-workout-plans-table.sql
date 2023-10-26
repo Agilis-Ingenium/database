a8034c35ea154e8951743a3be4f6f36b9168cb05
Warren Byron <20091892@mail.wit.ie>
2023-10-26 12:57:03 +0100

    Remove ENUM types

--- DDL/20-workout-plans-table.sql
+++ DDL/20-workout-plans-table.sql
@@ -2,7 +2,7 @@ CREATE TABLE workout_plans (
     plan_id SERIAL PRIMARY KEY,
     user_id INT NOT NULL,
     plan_name VARCHAR(255) NOT NULL,
-    schedule schedule,
+    schedule VARCHAR(20),
     plan_date DATE,
     FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
