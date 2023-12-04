b1c8ee280fdbb134ca24592cf781c10f2af919e3
Warren Byron <20091892@mail.wit.ie>
2023-12-04 16:40:28 +0000

    Update 05-fitness-goals-data.sql

--- DATA/05-fitness-goals-data.sql
+++ DATA/05-fitness-goals-data.sql
@@ -1,4 +1,4 @@
-INSERT INTO fitness_goals (user_id, goal_type, target_value, target_date)
+INSERT INTO fitness_goals (user_id, goal_type, target_value, target_date, achieved)
 VALUES
     (1, 'weight_loss', 10.0, '2023-12-31', false),
     (1, 'muscle_gain', 5.0, '2023-11-30', false),
