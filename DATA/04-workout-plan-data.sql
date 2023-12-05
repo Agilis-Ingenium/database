75410f67623ef650cd9d1adbd20506757d4fe1a8
Warren Byron <20091892@mail.wit.ie>
2023-12-05 16:47:13 +0000

    Update 04-workout-plan-data.sql

--- DATA/04-workout-plan-data.sql
+++ DATA/04-workout-plan-data.sql
@@ -1,6 +1,6 @@
 INSERT INTO workout_plans (user_id, plan_name, schedule, plan_date)
 VALUES
-    (1, 'Weight Training Plan', 'weekly', '2023-09-30'),
-    (1, 'Cardio Workout Plan', 'daily', '2023-10-01'),
-    (1, 'Cardio Workout Plan', 'bi-weekly', '2023-10-01'),
-    (1, 'Strength Training Plan', 'monthly', '2023-10-03');
+    (1, "Weight Training Plan", "weekly", "2023-09-29", "Weight Loss", "4 Weeks", "A comprehensive plan focused on cardio exercises, strength training, and a balanced diet."),
+    (1, "Cardio Workout Plan", "daily", "2023-09-30", "Cardiovascular Health", "6 Days", "A high-intensity cardio plan targeting overall cardiovascular health."),
+    (1, "Flexibility Plan", "bi-weekly", "2023-09-30", "Flexibility Improvement", "2 Weeks", "Focuses on yoga-based routines to enhance flexibility and reduce stiffness."),
+    (1, "Strength Training Plan", "monthly", "2023-10-02", "Muscle Gain", "1 Month", "Intense strength training regimen to stimulate muscle growth and strength."),
