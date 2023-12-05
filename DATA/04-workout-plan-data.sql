a655c2ed3345c5c363f91b4e190787d4d09ceb79
Warren Byron <20091892@mail.wit.ie>
2023-12-05 16:48:17 +0000

    Update 04-workout-plan-data.sql

--- DATA/04-workout-plan-data.sql
+++ DATA/04-workout-plan-data.sql
@@ -3,4 +3,4 @@ VALUES
     (1, "Weight Training Plan", "weekly", "2023-09-29", "Weight Loss", "4 Weeks", "A comprehensive plan focused on cardio exercises, strength training, and a balanced diet."),
     (1, "Cardio Workout Plan", "daily", "2023-09-30", "Cardiovascular Health", "6 Days", "A high-intensity cardio plan targeting overall cardiovascular health."),
     (1, "Flexibility Plan", "bi-weekly", "2023-09-30", "Flexibility Improvement", "2 Weeks", "Focuses on yoga-based routines to enhance flexibility and reduce stiffness."),
-    (1, "Strength Training Plan", "monthly", "2023-10-02", "Muscle Gain", "1 Month", "Intense strength training regimen to stimulate muscle growth and strength."),
+    (1, "Strength Training Plan", "monthly", "2023-10-02", "Muscle Gain", "1 Month", "Intense strength training regimen to stimulate muscle growth and strength.")
